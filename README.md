ab42fd0774e5131ca3ad8d9ac30ff96f3293402d
leoparaguassu1 <leoparaguassu1@gmail.com>
2021-03-29 10:18:29 -0300

    ETL_REPOTS ROX

---
+++ README.md
@@ -0,0 +1,6 @@
+# CSV2SQL
+
+Simple python tool to create SQL `INSERT` statements from CSV files.
+
+## Usage
+`python csv2sql.py -t <tablename> <csv file>`
---
+++ Sales.SalesOrderDetail.py
@@ -0,0 +1,40 @@
+from sqlalchemy import create_engine
+import urllib
+import pyodbc
+import pandas as pd
+
+
+
+
+conn = pyodbc.connect("DRIVER={SQL Server Native Client 11.0};"
+                                 "SERVER=LEO;"
+                                 "DATABASE=roxpartner;"
+                                 "UID=carga_dw;"
+                                 "PWD=carga_dw")
+
+cursor = conn.cursor()
+
+cursor.execute('DROP TABLE  roxpartner.dbo.SalesOrderDetail')
+conn.commit()
+
+
+
+df = pd.read_csv("Sales.SalesOrderDetail.csv" , sep=";", engine='python')
+
+
+params = urllib.parse.quote_plus("DRIVER={SQL Server Native Client 11.0};"
+                                 "SERVER=LEO;"
+                                 "DATABASE=roxpartner;"
+                                 "UID=carga_dw;"
+                                 "PWD=carga_dw")
+
+engine = create_engine("mssql+pyodbc:///?odbc_connect={}".format(params))
+
+
+
+
+df.to_sql('SalesOrderDetail', schema='dbo', con = engine)
+
+
+#result = engine.execute('SELECT COUNT(*) FROM [dbo].[Person]')
+#result.fetchall()
